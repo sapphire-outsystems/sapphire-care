8f3a0dd2a9049f39d57900e65c1128c63222d0b2
msgpt <msgpt@msn.com>
2020-05-05 10:42:14 +0100

    Many changes

--- ServiceStudioScript.js
+++ ServiceStudioScript.js
@@ -3,10 +3,33 @@ var localHostUrl = 'https://localhost:3000/dev.app.css';
 var add = (function() {
 	return {
 		checkLocalHost: function(params) {
-			this.removeCss();
-			this.localHostDev();
-		},
+			var _this = this;
 
+			fetch(localHostUrl, { mode: 'no-cors', })
+				.then(function(response) {
+					var checkIfExists = _this.getStyleSheet(localHostUrl);
+
+					if (checkIfExists) {
+						_this.localHostDev();
+					}
+
+					return;
+				})
+				.catch(function(error) {
+					console.log('Error: ', error);
+				});
+		},
+		getStyleSheet: function(unique_url) {
+			let query= document.querySelectorAll('style,link[rel="stylesheet"]');
+			let devLink = Array.from(query).filter(link=>link.href===unique_url);
+			if (devLink.length!==0){
+				return false;
+			}
+			else{
+				return true;
+			}
+			
+		},
 		removeCss: function() {
 			document.querySelectorAll('style,link[rel="stylesheet"]').forEach(function(item) {
 				return item.remove();
@@ -14,9 +37,9 @@ var add = (function() {
 		},
 		localHostDev: function() {
 			return (document.head.innerHTML +=
-				'<link rel="stylesheet" type="text/css" href="https://localhost:3000/dev.app.css" />');
+				`<link rel="stylesheet" type="text/css" href=${localHostUrl}/>`);
 		},
 	};
 })();
 
-add.localHostDev();
+add.checkLocalHost();
