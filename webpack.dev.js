567bab11fb2f19948c6e747649338e29c9c26e1c
msgpt <msgpt@msn.com>
2020-07-09 16:26:42 +0100

    Helper class fonts and webpack changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,11 +2,14 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const care_dev_url='https://atc-dev.outsystemsenterprise.com/Care_MUI'
+const style_dev_url='https://atc-dev.outsystemsenterprise.com/SapphireCare_StyleGuide/';
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports =env=>{
+	return merge(common, {
 	mode: 'development',
 	output: {
 		filename: 'dev.[name].js',
@@ -23,17 +26,6 @@ module.exports = merge(common, {
 		liveReload: false,
 		progress:true,
 		port: 3000,
-		//open:true,
-		// index:'',
-		// proxy: {
-		// 	'**': {
-		// 		target: 'https://atc-dev.outsystemsenterprise.com',
-		// 		openPage:'/Care_MUI',
-		// 		changeOrigin: true,
-		// 		secure: true,
-				
-		// 	}
-		// }
 	},
 	watch: true,
 	plugins: [
@@ -42,26 +34,16 @@ module.exports = merge(common, {
 			filename: 'dev.[name].css',
 		}),
 		new BrowserSyncPlugin(
-      // BrowserSync options
       {
-        // browse to http://localhost:3000/ during development
         host: 'localhost',
         port: 3000,
-        // proxy the Webpack Dev Server endpoint
-        // (which should be serving on http://localhost:3100/)
-				// through BrowserSync
-				proxy: 'https://atc-dev.outsystemsenterprise.com/SapphireCare_StyleGuide/'
-				//proxy: 'https://atc-dev.outsystemsenterprise.com/Care_MUI'
-        //proxy: 'https://miguelfilipe-gomes.outsystemscloud.com/ToDo_MFSG/'
+				proxy: env.url==='care'?care_dev_url:style_dev_url
       },
-      // plugin options
       {
-        // prevent BrowserSync from reloading the page
-        // and let Webpack Dev Server take care of this
 				reload: true
       }
 		)
 		
-	],
+	],	
 
-});
+})};
