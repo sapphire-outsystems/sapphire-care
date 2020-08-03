4dac8f6b25f22e74301cc683dcb6f95243660a46
Pablo Santos <pablo.santos@outsystems.com>
2020-08-03 18:01:06 +0100

    adjustments

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,48 +6,35 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const {CARE_URL,STYLEGUIDE_URL}=process.env;
+const { CARE_URL, STYLEGUIDE_URL } = process.env;
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
-module.exports =env=>{
+module.exports = env => {
 	return merge(common, {
-	mode: 'development',
-	output: {
-		filename: 'dev.[name].js',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		https: true,
-		watchContentBase: true,
-		writeToDisk: true,
-		disableHostCheck: true,
-		hot: true,
-		inline: true,
-		liveReload: false,
-		progress:true,
-		port: 3000,
-	},
-	watch: true,
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new MiniCssExtractPlugin({
-			filename: 'dev.[name].css',
-		}),
-		new BrowserSyncPlugin(
-      {
-        host: 'localhost',
-				port: 3000,
-				https: true,
-				proxy: env.url==='care'?CARE_URL:STYLEGUIDE_URL
-      },
-      {
-				reload: true
-      }
-		)
-		
-	],	
-
-})};
+		mode: 'development',
+		output: {
+			filename: 'dev.[name].js',
+		},
+		// devtool: 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			https: true,
+			watchContentBase: true,
+			writeToDisk: true,
+			disableHostCheck: true,
+			hot: true,
+			inline: true,
+			liveReload: false,
+			progress: true,
+			port: 3500,
+		},
+		watch: true,
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+			new MiniCssExtractPlugin({
+				filename: 'dev.[name].css',
+			}),
+		],
+	});
+};
